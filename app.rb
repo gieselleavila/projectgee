6c52dd834e2cfbfe4f225a25ffd113ecec6caea3
gieselleavila <avila.gieselle@gmail.com>
2016-03-04 17:54:06 +0000

    this shit is fire

---
+++ app.rb
@@ -0,0 +1,14 @@
+require_relative 'config/environment'
+class App < Sinatra::Base
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @text_from_user = params[:user_text]
+  erb :results
+end
+
+
+end
